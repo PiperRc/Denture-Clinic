3b310dbff7e71595160e6d5c5e5b6661e38d5ba1
Cheyenne Piper <robeltopiper@gmail.com>
2024-03-18 02:45:41 -0400

    Add nav effects when a link a clicked

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 let hamburger=document.querySelector('.hamburger');
 let nav=document.querySelector('nav');
 let navbar=document.querySelector('navbar');
+let navLinks=document.querySelectorAll('navbar a');
 
 
 hamburger.addEventListener('click',()=>{
@@ -8,3 +9,11 @@ hamburger.addEventListener('click',()=>{
   navbar.classList.toggle('navbar-display');
   hamburger.classList.toggle('hamburger-click');
 })
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.toggle('nav-width');
+    navbar.classList.toggle('navbar-display');
+    hamburger.classList.toggle('hamburger-click');
+  });
+});
