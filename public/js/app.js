01c89f76be414c1cab0ed445418d17122556b564
Cheyenne Piper <robeltopiper@gmail.com>
2024-03-22 20:41:04 -0400

    Place code required to change navbar into separate functions

--- public/js/app.js
+++ public/js/app.js
@@ -3,17 +3,20 @@ let nav = document.querySelector('nav');
 let navbar = document.querySelector('navbar');
 let navLinks = document.querySelectorAll('navbar a');
 
-hamburger.addEventListener('click',()=>{
+hamburger.addEventListener('click',toggleMenu)
+
+navLinks.forEach((link)=>{
+link.addEventListener('click',removeMenu)
+})
+
+function toggleMenu(){
   nav.classList.toggle('nav-width');
   navbar.classList.toggle('navbar-display');
   hamburger.classList.toggle('hamburger-click');
-})
+}
 
-navLinks.forEach((link)=>{
-link.addEventListener('click',()=>{
+function removeMenu(){
   nav.classList.remove('nav-width');
   navbar.classList.remove('navbar-display');
   hamburger.classList.remove('hamburger-click');
-
-})
-})
+}
